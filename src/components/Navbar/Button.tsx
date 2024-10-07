e3bb0fb81f5ccee6739d6c313283ab90e16d40da
nekzt-dev <contatonekzt@gmail.com>
2024-10-07 11:53:21 -0300

    refactor: change button

--- src/components/Navbar/Button.tsx
+++ src/components/Navbar/Button.tsx
@@ -10,18 +10,18 @@ export default function Button({ text }: IPropsButton) {
 
   return (
     <button
-      className="px-4 py-1 relative text-3xl gap-2 cursor-pointer rounded-sm overflow-hidden"
+      className="flex justify-center items-center px-4 py-1 relative text-3xl gap-2 cursor-pointer rounded-sm overflow-hidden"
       onMouseEnter={() => setIsAnimationButton(true)}
       onMouseLeave={() => setIsAnimationButton(false)}
     >
-      <div className={`${isAnimationButton ? 'w-full' : 'w-1'} transition-all z-0 h-full bg-zinc-200 absolute top-0 left-0`} />
-      {/* <div className="w-5 h-5 flex relative items-center justify-center">
-        <div className="bg-white w-6 h-6 z-10 absolute rounded-full opacity-20"></div>
-        <div className="bg-white w-4 h-4 z-20 absolute rounded-full opacity-25"></div>
+      {/* <div className={`${isAnimationButton ? 'w-full' : 'w-1'} transition-all z-0 h-full bg-zinc-200 absolute top-0 left-0`} /> */}
+      <div className="w-5 h-5 flex relative items-center justify-center">
+        <div className="bg-white w-6 h-6 z-10 absolute rounded-full opacity-20 animate-ping"></div>
+        <div className="bg-white w-4 h-4 z-20 absolute rounded-full opacity-20"></div>
         <div className="bg-white w-2 h-2 z-30 absolute rounded-full opacity-100"></div>
-      </div> */}
+      </div>
       <p
-        className={`${isAnimationButton ? 'text-slate-900' : 'text-zinc-200'} transition-all z-10 relative`}
+        className={`transition-all z-10 relative`}
       >
         {text}
       </p>
